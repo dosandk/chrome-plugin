889607f0e28c793aeac47ac4d2ca44df9f7ee8e0
Vladimir <dosandkv@gmail.com>
2017-03-26 22:57:46 +0300

    wrapped in IIFE;

--- plugin/app.js
+++ plugin/app.js
@@ -1,29 +1,31 @@
-const taskIdContainer = document.getElementById('key-val');
-const taskDescriptionContainer = document.getElementById('summary-val');
-const { textContent: taskId } = taskIdContainer;
-const { textContent: taskDescription } = taskDescriptionContainer;
+(function() {
+  const taskIdContainer = document.getElementById('key-val');
+  const taskDescriptionContainer = document.getElementById('summary-val');
+  const { textContent: taskId } = taskIdContainer;
+  const { textContent: taskDescription } = taskDescriptionContainer;
 
-const taskData = `${taskId} ${taskDescription}`;
+  const taskData = `${taskId} ${taskDescription}`;
 
-function copyToClipboard(text) {
-  const textarea = createElement('textarea');
+  function copyToClipboard(text) {
+    const textarea = createElement('textarea');
 
-  textarea.textContent = text;
-  document.body.appendChild(textarea);
-  textarea.select();
+    textarea.textContent = text;
+    document.body.appendChild(textarea);
+    textarea.select();
 
-  try {
-    document.execCommand('copy');
-    console.info('Task description was successful copied!')
-  } catch (e) {
-    console.error('Copy to clipboard failed!', e);
-  } finally {
-    document.body.removeChild(textarea);
+    try {
+      document.execCommand('copy');
+      console.info('Task description was successful copied!')
+    } catch (e) {
+      console.error('Copy to clipboard failed!', e);
+    } finally {
+      document.body.removeChild(textarea);
+    }
+
+    function createElement(elementName) {
+      return document.createElement(elementName);
+    }
   }
 
-  function createElement(elementName) {
-    return document.createElement(elementName);
-  }
-}
-
-copyToClipboard(taskData);
+  copyToClipboard(taskData);
+}());
